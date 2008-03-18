716fa369d52820f139d29aa4a71d89866bae12dd
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-03-18 08:01:48 +0000

    增加反斜线转义

--- includes/library/Db/DbAdapter/Mysql.php
+++ includes/library/Db/DbAdapter/Mysql.php
@@ -83,7 +83,7 @@ class TypechoMysql implements TypechoDbAdapter
      */
     public function quotes($string)
     {
-        return str_replace("'", "''", $string);
+        return str_replace(array('\'', '\\'), array('\'\'', '\\\\'), $string);
     }
 
     /**
