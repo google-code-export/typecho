72a44d90030acd812be07495ca6c47f5f1f5fccf
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-03-31 14:23:19 +0000

    更新部分注释

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 /** 载入项目配置文件 **/
 require 'includes/config.php';
 
-widget('Options')->to($options);
-widget('Access')->to($access);
+widget('Options')->to($options);    //初始化配置组件
+widget('Access')->to($access);      //初始化权限组件
 
 require TypechoRoute::target('./var/template/' . $options->template);
