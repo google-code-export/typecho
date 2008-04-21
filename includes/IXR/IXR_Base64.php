750e7f008106223a01e2eff9a8bc947cf59ea5e5
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-04-21 09:18:46 +0000

    分离IXR和PHPMailer包

---
+++ includes/IXR/IXR_Base64.php
@@ -0,0 +1,23 @@
+<?php
+/* 
+   IXR - The Inutio XML-RPC Library - (c) Incutio Ltd 2002
+   Version 1.61 - Simon Willison, 11th July 2003 (htmlentities -> htmlspecialchars)
+   Site:   http://scripts.incutio.com/xmlrpc/
+   Manual: http://scripts.incutio.com/xmlrpc/manual.php
+   Made available under the Artistic License: http://www.opensource.org/licenses/artistic-license.php
+*/
+
+/**
+ * IXR Base64编码
+ *
+ * @package IXR
+ */
+class IXR_Base64 {
+    var $data;
+    function IXR_Base64($data) {
+        $this->data = $data;
+    }
+    function getXml() {
+        return '<base64>'.base64_encode($this->data).'</base64>';
+    }
+}
