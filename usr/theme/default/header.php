2eba0df1dbe1977117a14c182f82393f0ea5eb6d
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-06-20 13:51:30 +0000

    替换默认皮肤

---
+++ usr/theme/default/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/2002/REC-xhtml1-20020801/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Typecho</title>
+<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('css/960.gs.css'); ?>" />
+<link rel="stylesheet" type="text/css" media="all" href="<?php $this->options->themeUrl('css/style.css'); ?>" />
+<?php $this->header(); ?>
+</head>
+
+<body>
+<div class="container_16 clearfix">
+    <div id="header" class="clearfix">
+		<div class="grid_16">
+			<ul class="clearfix" id="nav_menu">
+				<li><a href="#" class="current">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Guestbook</a></li>
+				<li><a href="#">Contact</a></li>
+				<li><a href="#" style="float: right; margin-right: 0;">Login</a></li>
+			</ul>
+		</div>
+		<div class="grid_9">
+			<img src="images/header.png" width="50" height="50" alt="" style="float: left; margin: 5px 10px 0 0;" />
+	        <h1><a href="#"><?php $this->options->title() ?></a></h1>
+			<span><?php $this->options->description() ?></span>
+		</div>
+		<div class="grid_7">
+			<div id="search"><input type="text" class="text" size="40" /> <input type="submit" class="submit" value="Search" /></div>
+		</div>
+    </div><!-- end #header -->
---
+++ usr/theme/default/single.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+
+    <div class="grid_11" id="content">
+        <div class="post">
+			<div class="entry_main">
+				<div class="entry_title">
+	                <h2><a href="#">K2 Release Candidate 6 Released</a></h2>
+		        </div>
+			    <div class="entry_text">
+				    <p>PHP界的开发热潮可以用风起云涌来形容了，最新的WEB动向都可以在这里体会到。在我上大学那会还是流行开发论坛，后来流行开发博客，最近似乎开始流行框架了。我们亲爱的水水同学也加入到这水生火热的框架开发大军中了，那么我觉得作为一个PHP开发人员，有必要谈一谈我对框架的看法，希望对这些框架开发者有些启示。</p>
+				    <p>PHP界的开发热潮可以用风起云涌来形容了，最新的WEB动向都可以在这里体会到。在我上大学那会还是流行开发论坛，后来流行开发博客，最近似乎开始流行框架了。我们亲爱的水水同学也加入到这水生火热的框架开发大军中了，那么我觉得作为一个PHP开发人员，有必要谈一谈我对框架的看法，希望对这些框架开发者有些启示。</p>
+					<div class="single_data clearfix">
+						<h3>Related Articles</h3>
+						<ul id="related_post">
+							<li><a href="#">MySpace, a Virtual Mirror of Society</a></li>
+							<li><a href="#">Religion in the Modern world</a></li>
+							<li><a href="#">A 13-Year-Old is Not a Playboy Bunny</a></li>
+							<li><a href="#">Truth Behind the Mask</a></li>
+							<li><a href="#">The Business of Blogging and How It is Mostly Rubbish</a></li>
+						</ul>
+					</div>
+		        </div>
+				<div class="entry_data">70 on June 18, 2008 | Filed Under <a href="#">Blogging Basics</a> | <a href="#">Trackbacks</a> | <a href="#response">leave a response</a>.<br />Tags: <a href="#">Blog</a>, <a href="#">Typecho</a>, <a href="#">Wordpress</a></div>
+			</div>
+        </div>
+		<div id="comments">
+			<h4>5 Responses to "K2 Release Candidate 6 Released"</h4>
+			<ol id="comment_list">
+			<?php for($a=1; $a!=6; $a++) echo'
+				<li>
+					<div class="c_user">'.$a.' | <strong><a href="#">Fen</a></strong> at January 12th, 2008 at 1:58 am </div>
+					<div class="c_text">
+						<img src="http://www.gravatar.com/avatar.php?gravatar_id=d18d64bf2b1d256fdcb811cafbbfa487&amp;size=32" width="32" height="32" class="gravatar" alt="" />
+						<p>Wt? You are asking? I bet, if you start selling premium themes, you will be number one in the paid theme market. Honestly.</p>
+						<p>The most best thing about you is attention to details that tops all other designers out there. I would only say, “go for it!”.</p>
+					</div>
+				</li>
+			'; ?>
+			</ol>
+			<h4 id="response">Leave a Reply</h4>
+			<form method="post" action="" id="comment_form">
+				<p><input type="text" class="text" size="50" /><label>Name (Required)</label></p>
+				<p><input type="text" class="text" size="50" /><label>E-mail (Required *will not be published)</label></p>
+				<p><input type="text" class="text" size="50" /><label>Website</label></p>
+				<p><textarea rows="12" cols=""></textarea></p>
+				<p><input type="submit" value="Submit Comment" class="submit" /></p>
+			</form>
+		</div>
+    </div><!-- end #content-->
+	<?php include('sidebar.php'); ?>
+	<?php include('footer.php'); ?>
