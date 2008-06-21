f1d3c6876199a83c80d7cbc441d88077682ccafd
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-06-21 16:12:34 +0000

    增加评论归档支持

---
+++ var/Widget/Comments/Archive.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * 评论归档
+ * 
+ * @category typecho
+ * @package Widget
+ * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license GNU General Public License 2.0
+ * @version $Id$
+ */
+
+/**
+ * 评论归档组件
+ * 
+ * @category typecho
+ * @package Widget
+ * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license GNU General Public License 2.0
+ */
+class Widget_Comments_Archive extends Widget_Abstract_Comments
+{
+    /**
+     * 构造函数,根据内容归档评论
+     * 
+     * @access public
+     * @param integer $cid 内容主键
+     * @return void
+     */
+    public function __construct($cid)
+    {
+        /** 初始化评论 */
+        parent::__construct();
+        
+        $this->db->fetchAll($this->select()->where('table.comments.`status` = ?', 'approved')
+        ->where('table.contents.`cid` = ?', $cid)->group('table.comments.`coid`')
+        ->order('table.comments.`created`', TypechoDb::SORT_DESC), array($this, 'push'));
+    }
+}
---
+++ var/Widget/Comments/Archive/Pingback.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * 评论归档,仅显示广播
+ * 
+ * @category typecho
+ * @package Widget
+ * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license GNU General Public License 2.0
+ * @version $Id$
+ */
+
+/**
+ * 评论归档组件,仅显示广播
+ * 
+ * @category typecho
+ * @package Widget
+ * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license GNU General Public License 2.0
+ */
+class Widget_Comments_Archive_Pingback extends Widget_Abstract_Comments
+{
+    /**
+     * 构造函数,根据内容归档评论
+     * 
+     * @access public
+     * @param integer $cid 内容主键
+     * @return void
+     */
+    public function __construct($cid)
+    {
+        /** 初始化评论 */
+        parent::__construct();
+        
+        $this->db->fetchAll($this->select()->where('table.comments.`status` = ?', 'approved')
+        ->where('table.comments.`mode` = ?', 'pingback')
+        ->where('table.contents.`cid` = ?', $cid)->group('table.comments.`coid`')
+        ->order('table.comments.`created`', TypechoDb::SORT_DESC), array($this, 'push'));
+    }
+}
