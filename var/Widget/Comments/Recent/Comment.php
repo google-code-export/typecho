f9625f3e75e13a9dc85b47a69690a58377be06c7
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-06-25 02:03:23 +0000

    增加 Comments/Recent 组件

--- var/Widget/Comments/Recent/Comment.php
+++ var/Widget/Comments/Recent/Comment.php
@@ -8,15 +8,15 @@
  */
 
 /**
- * 最近评论组件
+ * 最近评论组件,只显示评论
  * 
  * @category typecho
  * @package Widget
  * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
  * @license GNU General Public License 2.0
  */
-class Widget_Comments_Recent extends Widget_Abstract_Comments
-{    
+class Widget_Comments_Recent_Comment extends Widget_Abstract_Comments
+{
     /**
      * 入口函数
      *
@@ -30,6 +30,7 @@ class Widget_Comments_Recent extends Widget_Abstract_Comments
         $pageSize = empty($pageSize) ? $this->options->commentsListSize : $pageSize;
         
         $this->db->fetchAll($this->select()->limit($pageSize)
+        ->where('table.comments.`type` = ?', 'comment')
         ->where('table.comments.`status` = ?', 'approved')
         ->group('table.comments.`coid`')
         ->order('table.comments.`created`', Typecho_Db::SORT_DESC), array($this, 'push'));
--- var/Widget/Comments/Recent/Pingback.php
+++ var/Widget/Comments/Recent/Pingback.php
@@ -8,15 +8,15 @@
  */
 
 /**
- * 最近评论组件
+ * 最近评论组件,只显示广播
  * 
  * @category typecho
  * @package Widget
  * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
  * @license GNU General Public License 2.0
  */
-class Widget_Comments_Recent extends Widget_Abstract_Comments
-{    
+class Widget_Comments_Recent_Pingback extends Widget_Abstract_Comments
+{
     /**
      * 入口函数
      *
@@ -30,6 +30,7 @@ class Widget_Comments_Recent extends Widget_Abstract_Comments
         $pageSize = empty($pageSize) ? $this->options->commentsListSize : $pageSize;
         
         $this->db->fetchAll($this->select()->limit($pageSize)
+        ->where('table.comments.`type` = ?', 'pingback')
         ->where('table.comments.`status` = ?', 'approved')
         ->group('table.comments.`coid`')
         ->order('table.comments.`created`', Typecho_Db::SORT_DESC), array($this, 'push'));
