f3d4227d9472ad8fb520d71a1a8345b76d0e6f95
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-07-05 05:07:23 +0000

    增加描述性语言

--- trunk/var/Widget/Options/Discussion.php
+++ trunk/var/Widget/Options/Discussion.php
@@ -39,7 +39,8 @@ class Widget_Options_Discussion extends Widget_Abstract_Options implements Widge
         
         /** 评论日期格式 */
         $commentDateFormat = new Typecho_Widget_Helper_Form_Text('commentDateFormat', $this->commentDateFormat,
-        _t('评论日期格式'), _t('请参考<a href="http://cn.php.net/manual/zh/function.date.php" target="_blank">PHP日期格式写法</a>.'));
+        _t('评论日期格式'), _t('这是一个默认的格式,当你在模板中调用显示评论日期方法时,如果没有指定日期格式,将按照此格式输出.<br />
+        具体写法请参考<a href="http://cn.php.net/manual/zh/function.date.php" target="_blank">PHP日期格式写法</a>.'));
         $commentDateFormat->input->setAttribute('class', 'text')->setAttribute('style', 'width:40%');
         $form->addInput($commentDateFormat);
         
@@ -51,17 +52,24 @@ class Widget_Options_Discussion extends Widget_Abstract_Options implements Widge
         
         /** 是否在列表中的评论者处显示其个人主页链接 */
         $commentsShowUrl = new Typecho_Widget_Helper_Form_Radio('commentsShowUrl', array('0' => _t('不显示'), '1' => _t('显示')),
-        $this->commentsShowUrl, _t('是否在列表中的评论者名称处显示其个人主页链接'));
+        $this->commentsShowUrl, _t('是否在列表中的评论者名称处显示其个人主页链接'),
+        _t('如果你打开此选项,当评论作者在提交评论时留下的个人主页地址,将以链接的形式呈现出来.<br />
+        在某些主题中此选项可能不会生效,因为它可以在模板中被强行设置.'));
         $form->addInput($commentsShowUrl);
         
-        /** 是否对评论者个人链接使用nofollow属性 */
+        /** 是否对评论者个人主页链接使用nofollow属性 */
         $commentsUrlNofollow = new Typecho_Widget_Helper_Form_Radio('commentsUrlNofollow', array('0' => _t('不启用'), '1' => _t('启用')),
-        $this->commentsUrlNofollow, _t('是否对评论者个人链接使用nofollow属性'));
+        $this->commentsUrlNofollow, _t('是否对评论者个人主页链接使用nofollow属性'),
+        _t('当评论作者的个人主页地址在你的网站上呈现时,其在搜索引擎中可能被识别为外链地址.<br />
+        过多的外链地址将导致你的网站在搜索引擎中被降权,打开此选项能帮助你解决此问题.<br />
+        更多关于nofollow的信息请参考<a href="http://en.wikipedia.org/wiki/Nofollow" target="_blank">wikipedia上的解释</a>.'));
         $form->addInput($commentsUrlNofollow);
         
         /** 评论审核 */
         $commentsRequireModeration = new Typecho_Widget_Helper_Form_Radio('commentsRequireModeration', array('0' => _t('不需要'), '1' => _t('需要')),
-        $this->commentsRequireModeration, _t('评论审核'));
+        $this->commentsRequireModeration, _t('评论审核'),
+        _t('打开此选项后,所有提交的评论,引用通告和广播将不会立即呈现,而是被标记为待审核,你可以在后台标记它们是否呈现.<br />
+        被评论文章的作者和编辑及以上权限的用户不受此选项的约束.'));
         $form->addInput($commentsRequireModeration);
         
         /** 在文章发布一段时间后自动关闭评论和广播功能 */
