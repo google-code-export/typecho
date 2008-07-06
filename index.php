d8aead6845a81f6e488c7ba7539e195236987656
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-07-06 11:27:26 +0000

    修正插件载入

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
 /** 载入配置支持 */
 require_once 'config.inc.php';
 
+/** 初始化插件 */
+Typecho_Plugin::init(Typecho_API::factory('Widget_Abstract_Options')->plugins,
+__TYPECHO_ROOT_DIR__ . '/' . __TYPECHO_PLUGIN_DIR__);
+
 /** 开始路由分发 */
 Typecho_Router::dispatch();
