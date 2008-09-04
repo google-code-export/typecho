4ab1239b6f5fdea977675b9d9ee3a53a67507498
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-09-04 13:22:42 +0000

    增加xmlrpc接口

--- var/IXR/Server.php
+++ var/IXR/Server.php
@@ -30,12 +30,50 @@ require_once 'IXR/Base64.php';
  *
  * @package IXR
  */
-class IXR_Server {
-    var $data;
-    var $callbacks = array();
-    var $message;
-    var $capabilities;
-    function IXR_Server($callbacks = false, $data = false) {
+class IXR_Server
+{
+    /**
+     * 输入参数
+     * 
+     * @access private
+     * @var array
+     */
+    private $data;
+    
+    /**
+     * 回调函数
+     * 
+     * @access private
+     * @var array
+     */
+    private $callbacks = array();
+    
+    /**
+     * 消息体
+     * 
+     * @access private
+     * @var IXR_Message
+     */
+    private $message;
+    
+    /**
+     * 默认参数
+     * 
+     * @access private
+     * @var array
+     */
+    private $capabilities;
+    
+    /**
+     * 构造函数
+     * 
+     * @access public
+     * @param mixed $callbacks 回调函数
+     * @param mixed $data 输入参数
+     * @return void
+     */
+    public function __construct($callbacks = false, $data = false)
+    {
         $this->setCapabilities();
         if ($callbacks) {
             $this->callbacks = $callbacks;
@@ -43,7 +81,150 @@ class IXR_Server {
         $this->setCallbacks();
         $this->serve($data);
     }
-    function serve($data = false) {
+    
+    /**
+     * 呼叫内部方法
+     * 
+     * @access private
+     * @param string $methodname 方法名
+     * @param mixed $args 参数
+     * @return mixed
+     */
+    private function call($methodname, $args)
+    {
+        if (!$this->hasMethod($methodname)) {
+            return new IXR_Error(-32601, 'server error. requested method '.$methodname.' does not exist.');
+        }
+        $method = $this->callbacks[$methodname];
+        // Perform the callback and send the response
+        if (count($args) == 1) {
+            // If only one paramater just send that instead of the whole array
+            $args = $args[0];
+        }
+        // Are we dealing with a function or a method?
+        if (substr($method, 0, 5) == 'this:') {
+            // It's a class method - check it exists
+            $method = substr($method, 5);
+            if (!method_exists($this, $method)) {
+                return new IXR_Error(-32601, 'server error. requested class method "'.$method.'" does not exist.');
+            }
+            // Call the method
+            $result = $this->$method($args);
+        } 
+        if(is_array($method))
+        {
+            list($object, $func) = $method;
+            if (!is_callable($method)) {
+                return new IXR_Error(-32601, 'server error. requested class method "'.$object . '.' . $func.'" does not exist.');
+            }
+            $result = call_user_func_array(array($object, $func), $args);
+        }
+        else {
+            // It's a function - does it exist?
+            if (!function_exists($method)) {
+                return new IXR_Error(-32601, 'server error. requested function "'.$method.'" does not exist.');
+            }
+            // Call the function
+            $result = $method($args);
+        }
+        return $result;
+    }
+
+    /**
+     * 抛出错误
+     * 
+     * @access private
+     * @param integer $error 错误代码
+     * @param string $message 错误消息
+     * @return void
+     */
+    private function error($error, $message = false)
+    {
+        // Accepts either an error object or an error code and message
+        if ($message && !is_object($error)) {
+            $error = new IXR_Error($error, $message);
+        }
+        $this->output($error->getXml());
+    }
+    
+    /**
+     * 输出xml
+     * 
+     * @access private
+     * @param string $xml 输出xml
+     * @return 输出xml
+     */
+    private function output($xml)
+    {
+        $xml = '<?xml version="1.0"?>'."\n".$xml;
+        $length = strlen($xml);
+        header('Connection: close');
+        header('Content-Length: '.$length);
+        header('Content-Type: text/xml');
+        header('Date: '.date('r'));
+        echo $xml;
+        exit;
+    }
+    
+    /**
+     * 是否存在方法
+     * 
+     * @access private
+     * @param string $method 方法名
+     * @return mixed
+     */
+    private function hasMethod($method)
+    {
+        return in_array($method, array_keys($this->callbacks));
+    }
+    
+    /**
+     * 设置默认参数
+     * 
+     * @access public
+     * @return void
+     */
+    private function setCapabilities()
+    {
+        // Initialises capabilities array
+        $this->capabilities = array(
+            'xmlrpc' => array(
+                'specUrl' => 'http://www.xmlrpc.com/spec',
+                'specVersion' => 1
+            ),
+            'faults_interop' => array(
+                'specUrl' => 'http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php',
+                'specVersion' => 20010516
+            ),
+            'system.multicall' => array(
+                'specUrl' => 'http://www.xmlrpc.com/discuss/msgReader$1208',
+                'specVersion' => 1
+            ),
+        );   
+    }
+    
+    /**
+     * 设置默认方法
+     * 
+     * @access private
+     * @return void
+     */
+    private function setCallbacks()
+    {
+        $this->callbacks['system.getCapabilities'] = 'this:getCapabilities';
+        $this->callbacks['system.listMethods'] = 'this:listMethods';
+        $this->callbacks['system.multicall'] = 'this:multiCall';
+    }
+    
+    /**
+     * 服务入口
+     * 
+     * @access private
+     * @param mixed $data 输入参数
+     * @return void
+     */
+    private function serve($data = false)
+    {
         if (!$data) {
             global $HTTP_RAW_POST_DATA;
             if (!$HTTP_RAW_POST_DATA) {
@@ -82,93 +263,42 @@ EOD;
         // Send it
         $this->output($xml);
     }
-    function call($methodname, $args) {
-        if (!$this->hasMethod($methodname)) {
-            return new IXR_Error(-32601, 'server error. requested method '.$methodname.' does not exist.');
-        }
-        $method = $this->callbacks[$methodname];
-        // Perform the callback and send the response
-        if (count($args) == 1) {
-            // If only one paramater just send that instead of the whole array
-            $args = $args[0];
-        }
-        // Are we dealing with a function or a method?
-        if (substr($method, 0, 5) == 'this:') {
-            // It's a class method - check it exists
-            $method = substr($method, 5);
-            if (!method_exists($this, $method)) {
-                return new IXR_Error(-32601, 'server error. requested class method "'.$method.'" does not exist.');
-            }
-            // Call the method
-            $result = $this->$method($args);
-        } 
-        if(is_array($method))
-        {
-            list($object, $func) = $method;
-            $result = call_user_func_array(array($object, $func), $args);
-        }
-        else {
-            // It's a function - does it exist?
-            if (!function_exists($method)) {
-                return new IXR_Error(-32601, 'server error. requested function "'.$method.'" does not exist.');
-            }
-            // Call the function
-            $result = $method($args);
-        }
-        return $result;
-    }
-
-    function error($error, $message = false) {
-        // Accepts either an error object or an error code and message
-        if ($message && !is_object($error)) {
-            $error = new IXR_Error($error, $message);
-        }
-        $this->output($error->getXml());
-    }
-    function output($xml) {
-        $xml = '<?xml version="1.0"?>'."\n".$xml;
-        $length = strlen($xml);
-        header('Connection: close');
-        header('Content-Length: '.$length);
-        header('Content-Type: text/xml');
-        header('Date: '.date('r'));
-        echo $xml;
-        exit;
-    }
-    function hasMethod($method) {
-        return in_array($method, array_keys($this->callbacks));
-    }
-    function setCapabilities() {
-        // Initialises capabilities array
-        $this->capabilities = array(
-            'xmlrpc' => array(
-                'specUrl' => 'http://www.xmlrpc.com/spec',
-                'specVersion' => 1
-            ),
-            'faults_interop' => array(
-                'specUrl' => 'http://xmlrpc-epi.sourceforge.net/specs/rfc.fault_codes.php',
-                'specVersion' => 20010516
-            ),
-            'system.multicall' => array(
-                'specUrl' => 'http://www.xmlrpc.com/discuss/msgReader$1208',
-                'specVersion' => 1
-            ),
-        );   
-    }
-    function getCapabilities($args) {
+    
+    /**
+     * 获取默认参数
+     * 
+     * @access public
+     * @param mixed $args 输入参数
+     * @return array
+     */
+    public function getCapabilities($args)
+    {
         return $this->capabilities;
     }
-    function setCallbacks() {
-        $this->callbacks['system.getCapabilities'] = 'this:getCapabilities';
-        $this->callbacks['system.listMethods'] = 'this:listMethods';
-        $this->callbacks['system.multicall'] = 'this:multiCall';
-    }
-    function listMethods($args) {
+    
+    /**
+     * 列出所有方法
+     * 
+     * @access public
+     * @param mixed $args 输入参数
+     * @return mixed
+     */
+    public function listMethods($args)
+    {
         // Returns a list of methods - uses array_reverse to ensure user defined
         // methods are listed before server defined methods
         return array_reverse(array_keys($this->callbacks));
     }
-    function multiCall($methodcalls) {
+    
+    /**
+     * 一次处理多个请求
+     * 
+     * @access public
+     * @param void $methodcalls
+     * @return array
+     */
+    public function multiCall($methodcalls)
+    {
         // See http://www.xmlrpc.com/discuss/msgReader$1208
         $return = array();
         foreach ($methodcalls as $call) {
---
+++ var/Widget/XmlRpc.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Typecho Blog Platform
+ *
+ * @copyright  Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license    GNU General Public License 2.0
+ * @version    $Id: DoWidget.php 122 2008-04-17 10:04:27Z magike.net $
+ */
+
+/**
+ * XmlRpc接口
+ * 
+ * @author qining
+ * @category typecho
+ * @package Widget_XmlRpc
+ * @copyright Copyright (c) 2008 Typecho team (http://www.typecho.org)
+ * @license GNU General Public License 2.0
+ */
+class Widget_XmlRpc extends Typecho_Widget implements Widget_Interface_Action_Widget
+{
+    /**
+     * 入口执行方法
+     * 
+     * @access public
+     * @return void
+     */
+    public function action()
+    {
+        
+    }
+}
