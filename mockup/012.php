f3883de746132a8e02b0227bf428c26a658abf74
i.feelinglucky <i.feelinglucky@0318ff09-9246-0410-8118-259958648ee7>
2008-09-27 10:49:54 +0000

    更新新版后台模板样式

--- mockup/012.php
+++ mockup/012.php
@@ -5,6 +5,7 @@
         <div class="column-14 start-06 typecho-install">
             <h1 class="typecho-install-title">开始安装</h1>
                 <?php include 'slice/typecho-install-body.inc.html'; ?>
+            <p class="submit"><button>我同意，并准备好了</button></p>
         </div>
     </div>
 </div>
