d61f0820546aa46a9bff15bf5220f767e30970fd
i.feelinglucky <i.feelinglucky@0318ff09-9246-0410-8118-259958648ee7>
2008-09-27 03:39:17 +0000

    更新新版后台模板样式

---
+++ trunk/mockup/012.php
@@ -0,0 +1,12 @@
+<?php include 'slice/header.inc.html'; ?>
+<?php include 'slice/typecho-install-path.inc.html'; ?>
+<div class="body body-950">
+    <div class="container">
+        <div class="column-14 start-06 typecho-install">
+            <h1 class="typecho-install-title">安装简介</h1>
+                <?php include 'slice/typecho-install-body.inc.html'; ?>
+            <p class="submit"><button>我同意，并让我们开始吧！</button></p>
+        </div>
+    </div>
+</div>
+<?php include 'slice/footer.inc.html'; ?>
