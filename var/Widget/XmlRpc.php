a1bb2fa3b4af7db2bdce1a0f35c34ea02af4fc15
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-10-04 18:30:23 +0000

    开始对数据层做进一步抽象,增加对象复用性 将常用query抽象出来,而不是传统的model构建数据层方式

--- var/Widget/XmlRpc.php
+++ var/Widget/XmlRpc.php
@@ -19,6 +19,72 @@
 class Widget_XmlRpc extends Widget_Abstract_Contents implements Widget_Interface_Action_Widget
 {
     /**
+     * 用户结构 
+     * 
+     * @var mixed
+     * @access private
+     */
+    private $_user;
+
+    /**
+     * 构造函数 
+     * 
+     * @access public
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->_user = Typecho_API::factory('Widget_Users_Current');
+    }
+
+    /**
+     * checkAccess  检测用户权限 
+     * 
+     * @param mixed $userName 用户名
+     * @param mixed $password 密码
+     * @access private
+     * @return void
+     */
+    private function checkAccess($userName, $password)
+    {
+        /** 开始验证用户 **/
+        $user = $this->db->fetchRow($this->select()
+        ->where('`name` = ?', $userName)
+        ->limit(1));
+
+        if($user && $user['password'] == md5($password))
+        {
+            $this->_user->login($user['uid'], $user['password'], sha1(Typecho_API::randString(20)), 0);
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * wpGetPage: wordpress获取页面信息的方法 
+     * 
+     * @param mixed $blogId 博客id
+     * @param mixed $pageId 页面id
+     * @param mixed $userName 用户名
+     * @param mixed $password 密码
+     * @access public
+     * @return void
+     */
+    public function wpGetPage($blogId, $pageId, $userName, $password)
+    {
+        if($this->checkAccess($userName, $password))
+        {
+            return new IXR_Error(403, _t('权限禁止'));
+        }
+        
+        
+    }
+
+    /**
      * 入口执行方法
      * 
      * @access public
@@ -26,6 +92,10 @@ class Widget_XmlRpc extends Widget_Abstract_Contents implements Widget_Interface
      */
     public function action()
     {
+        $methods = array(
+                'wp.getPage'    =>  array($this, 'wpGetPage'),
+                );
+
         if(isset($_GET['rsd']))
         {
             echo '<?xml version="1.0" encoding="' . $this->options->charset .'"?>'; ?>
