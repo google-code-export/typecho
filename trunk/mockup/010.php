17ce59fc28fe42c046938b802601bdf9164bbf6f
i.feelinglucky <i.feelinglucky@0318ff09-9246-0410-8118-259958648ee7>
2008-10-21 14:22:21 +0000

    更新 foot

--- trunk/mockup/010.php
+++ trunk/mockup/010.php
@@ -20,10 +20,12 @@
     </div>
 </div>
 <?php include 'slice/typecho-head-guid.inc.html'; ?>
-<div class="body body-950">
-    <?php include 'slice/typecho-page-title.inc.html'; ?>
-    <div class="container typecho-page-main">
-        <?php include 'slice/typecho-list-table.inc.html'; ?>
+<div class="main">
+    <div class="body body-950">
+        <?php include 'slice/typecho-page-title.inc.html'; ?>
+        <div class="container typecho-page-main">
+            <?php include 'slice/typecho-list-table.inc.html'; ?>
+        </div>
     </div>
 </div>
 <?php include 'slice/typecho-foot.inc.html'; ?>
