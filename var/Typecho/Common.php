2ce2795d2945882e892801f75ee31f0d4df0b9de
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-10-24 11:32:10 +0000

    增加hash算法，并兼容老md5算法

--- var/Typecho/Common.php
+++ var/Typecho/Common.php
@@ -428,13 +428,68 @@ class Typecho_Common
      */
     public static function randString($length)
     {
-        $str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
         $result = '';
         for($i = 0; $i < $length; $i++)
         {
-            $result .= $str[rand(0, 52)];
+            $result .= ord(rand(32, 126));
         }
         return $result;
+    }
+    
+    /**
+     * 对字符串进行hash加密
+     * 
+     * @access public
+     * @param string $string 需要hash的字符串
+     * @param string $salt 扰码
+     * @return string
+     */
+    public static function hash($string, $salt = NULL)
+    {
+        /** 生成随机字符串 */
+        $salt = empty($salt) ? self::randString(9) : $salt;
+        $length = strlen($string);
+        $hash = '';
+        $last = ord($string[$length - 1]);
+        $pos = 0;
+        
+        /** 判断扰码长度 */
+        if(strlen($salt) != 9)
+        {
+            /** 如果不是9直接返回 */
+            return;
+        }
+        
+        while($pos < $length)
+        {
+            $asc = ord($string[$pos]);
+            $last = ($last * ord($salt[($last % $asc) % 9]) + $asc) % 95 + 32;
+            $hash .= chr($last);
+            $pos ++;
+        }
+
+        return '$T$' . $salt . md5($hash);
+    }
+    
+    /**
+     * 判断hash值是否相等
+     * 
+     * @access public
+     * @param string $from 源字符串
+     * @param string $to 目标字符串
+     * @return boolean
+     */
+    public static function hashValidate($from, $to)
+    {
+        if('$T$' == substr($to, 0, 3))
+        {
+            $salt = substr($to, 0, 12);
+            return self::hash($from, $salt) == $to;
+        }
+        else
+        {
+            return md5($from) == $to;
+        }
     }
 
     /**
