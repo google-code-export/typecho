86286851b1f4795ee02b0952a2b12f62c7d467af
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-10-30 16:13:39 +0000

    精简plugin,大大提高效率 如果需要正常运行,请暂时不要更新到此版本

--- var/Typecho/Plugin.php
+++ var/Typecho/Plugin.php
@@ -8,19 +8,6 @@
  */
 
 /**
- * Typecho_Plugin::instance别名
- * 
- * @access public
- * @param string $fileName 句柄
- * @return TypechoPlugin
- * @throws TypechoPluginException
- */
-function _p($handle, $adapterName)
-{
-    return Typecho_Plugin::factory($handle, ucfirst($adapterName));
-}
-
-/**
  * 插件处理类
  *
  * @category typecho
@@ -31,63 +18,215 @@ function _p($handle, $adapterName)
 class Typecho_Plugin
 {
     /**
-     * 适配器列表
+     * 所有激活的插件
      * 
      * @access private
      * @var array
      */
-    private static $_adapters = array();
+    private static $_plugins = array();
+    
+    /**
+     * 已经加载的文件
+     * 
+     * @access private
+     * @var array
+     */
+    private static $_required = array();
+    
+    /**
+     * 实例化的插件对象
+     * 
+     * @access private
+     * @var array
+     */
+    private static $_instances;
+    
+    /**
+     * 临时存储变量
+     * 
+     * @access private
+     * @var array
+     */
+    private static $_tmp = array();
+    
+    /**
+     * 唯一句柄
+     * 
+     * @access private
+     * @var string
+     */
+    private $_handle;
+    
+    /**
+     * 组件
+     * 
+     * @access private
+     * @var string
+     */
+    private $_component;
+    
+    /**
+     * 插件初始化
+     * 
+     * @access public
+     * @param string $handle 插件
+     * @return void
+     */
+    public function __construct($handle)
+    {
+        /** 初始化变量 */
+        $this->_handle = $handle;
+    }
 
     /**
      * 插件初始化
      * 
      * @access public
      * @param array $plugins 插件列表
-     * @param string $rootPath 插件根目录
      * @return void
-     * @throws Typecho_Plugin_Exception
      */
-    public static function init(array $plugins, $rootPath)
+    public static function init(array $plugins)
     {
-        foreach($plugins as $pluginName)
-        {
-            $pluginFileName = $rootPath . '/' . $pluginName . '/Plugin.php';
-            if(file_exists($pluginFileName))
-            {
-                /** 载入插件主文件 */
-                require_once $pluginFileName;
-                
-                /** 运行初始化方法 */
-                call_user_func(array($pluginName . '_Plugin', 'init'));
-            }
-            else
-            {
-                /** 载入异常支持 */
-                require_once 'Typecho/Plugin/Exception.php';
-                /** 如果不存在则抛出异常 */
-                throw new Typecho_Plugin_Exception("Plugin '{$pluginFileName}' not found", Typecho_Exception::RUNTIME);
-            }
-        }
+        $plugins['activated'] = array_key_exists('activated', $plugins) ? $plugins['activated'] : array();
+        $plugins['handles'] = array_key_exists('handles', $plugins) ? $plugins['handles'] : array();
+        $plugins['files'] = array_key_exists('files', $plugins) ? $plugins['files'] : array();
+        
+        /** 初始化变量 */
+        self::$_plugins = $plugins;
     }
-
+    
     /**
-     * 根据唯一的文件名初始化一个plugin实例
+     * 获取实例化插件对象
+     * 
+     * @access public
+     * @return Typecho_Plugin
+     */
+    public static function factory($handle)
+    {
+        return isset(self::$_instances[$handle]) ? self::$_instances[$handle] : (self::$_instances[$handle] = new Typecho_Plugin($handle));
+    }
+    
+    /**
+     * 激活插件
+     * 
+     * @access public
+     * @param string $pluginName 插件名称
+     * @return void
+     */
+    public static function activate($pluginName)
+    {
+        self::$_plugins['activated'][$pluginName] = self::$_tmp;
+        self::$_tmp = array();
+    }
+    
+    /**
+     * 禁用插件
+     * 
+     * @access public
+     * @param string $pluginName 插件名称
+     * @return void
+     */
+    public static function deactivate($pluginName)
+    {
+        /** 去掉所有相关文件 */
+        foreach(self::$_plugins['activated'][$pluginName]['files'] as $handle => $files)
+        {
+            self::$_plugins['files'][$handle] = array_diff(self::$_plugins['files'][$handle], $files);
+        }
+        
+        /** 去掉所有相关回调函数 */
+        foreach(self::$_plugins['activated'][$pluginName]['handles'] as $handle => $handles)
+        {
+            self::$_plugins['handles'][$handle] = array_diff(self::$_plugins['handles'][$handle], $handles);
+        }
+        
+        /** 禁用当前插件 */
+        unset(self::$_plugins['activated'][$pluginName]);
+    }
+    
+    /**
+     * 导出当前插件设置
+     * 
+     * @access public
+     * @return array
+     */
+    public static function export()
+    {
+        return self::$_plugins;
+    }
+    
+    /**
+     * 需要预先包含的文件
+     * 
+     * @access public
+     * @param string $file 文件名称(相对路径)
+     * @return void
+     */
+    public function need($file)
+    {
+        $handle = $this->_handle . ':' . $this->_component;
+        self::$_plugins['files'][$handle][] = $file;
+        self::$_tmp['files'][$handle][] = $file;
+    }
+    
+    /**
+     * 设置回调函数
      * 
      * @access public
      * @param string $handle 句柄
-     * @return TypechoPlugin
-     * @throws TypechoPluginException
+     * @param mixed $value 回调函数
+     * @return void
      */
-    public static function factory($handle, $adapterName)
+    public function __set($handle, $value)
     {
-        $handle = trim($handle, '/ ');
-        if(empty(self::$_adapters[$handle]))
+        $handle = $this->_handle . ':' . $handle;
+        self::$_plugins['handles'][$handle][] = $value;
+        self::$_tmp['handles'][$handle][] = $value;
+    }
+    
+    /**
+     * 通过魔术函数设置当前组件位置
+     * 
+     * @access public
+     * @param string $component 当前组件
+     * @return void
+     */
+    public function __get($component)
+    {
+        $this->_component = $component;
+    }
+    
+    /**
+     * 回调处理函数
+     * 
+     * @access public
+     * @param string $handle 句柄
+     * @param string $args 参数
+     * @return mixed
+     */
+    public function __call($handle, $args)
+    {
+        $handle = $this->_handle . ':' . $handle;
+        $last = count($args);
+        
+        if(isset($this->_required[$handle]) && isset(self::$_plugins['files'][$handle]))
         {
-            require_once 'Typecho/Plugin/Adapter/' . $adapterName . '.php';
-            $adapterName = 'Typecho_Plugin_Adapter_' . $adapterName;
-            self::$_adapters[$handle] = new $adapterName();
+            $this->_required[$handle] = true;
+            foreach(self::$_plugins['files'][$handle] as $file)
+            {
+                require_once $file;
+            }
+        }
+    
+        if(isset(self::$_plugins['handles'][$handle]))
+        {
+            $args[$last] = NULL;
+            foreach(self::$_plugins['handles'][$handle] as $callback)
+            {
+                $args[$last] = call_user_func_array($callback, $args);
+            }
         }
         
-        return self::$_adapters[$handle];
+        return $args[$last];
     }
 }
