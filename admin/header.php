adccb56cd164e5d57dde5d44b3d447b328370cf6
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-11-10 06:04:01 +0000

    替换后台菜单为新样式 在mockup中应用新的input样式，并hack部分webkit平台的focus效果

--- admin/header.php
+++ admin/header.php
@@ -3,7 +3,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=<?php $options->charset(); ?>" />
-        <title><?php echo $title; ?></title>
+        <title><?php _e('%s - Powered by Typecho', $menu->title); ?></title>
         <link rel="stylesheet" type="text/css" href="<?php $options->adminUrl('css/reset.source.css'); ?>" />
         <link rel="stylesheet" type="text/css" href="<?php $options->adminUrl('css/grid.source.css'); ?>" />
         <link rel="stylesheet" type="text/css" href="<?php $options->adminUrl('css/typecho.source.css'); ?>" />
