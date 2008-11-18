a24e38bae1bda2c3a5d2b712c6db4939109d78cc
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-11-18 05:15:23 +0000

    修正滚动问题

--- admin/common-js.php
+++ admin/common-js.php
@@ -1,5 +1,6 @@
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <script type="text/javascript" src="<?php $options->adminUrl('javascript/mootools-1.2.1-core-yc.js'); ?>"></script>
+<script type="text/javascript" src="<?php $options->adminUrl('javascript/mootools-1.2.1-more.js'); ?>"></script>
 <script type="text/javascript"> 
     (function () {
         var typechoGuid = function (el, config) {
@@ -85,6 +86,12 @@
 
         window.addEvent('domready', function() {
             var handle = new typechoGuid('typecho:guid', {offset: 1, type: 'mouse'});
+            var firstError = $$('.error')[0];
+            
+            //增加滚动效果
+            if (firstError) {
+                var errorFx = new Fx.Scroll(window).toElement(firstError.getParent('.typecho-option'));
+            }
             handle.reSet();
         });
     })();
