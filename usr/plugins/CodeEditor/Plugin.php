fb010d97d941896fc68812dc329fca4df7358a89
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-12-05 02:29:09 +0000

    优化options调用规则 增加后台模板编辑器插件 fix issue 42

---
+++ usr/plugins/CodeEditor/Plugin.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * R&uuml;stet Syntax-Highlighting f&uuml;r den Theme- und Plugin-Editor nach. Unterst&uuml;tzt php, css, html und js. F&uuml;r weitere Infos besuch die <a href="http://www.naden.de/blog/wordpress-code-editor" target="_blank">Plugin Homepage</a>.
+ * 
+ * @package Typecho Code Editor 
+ * @author Naden Badalgogtapeh
+ * @version 1.1 / 30.05.2007
+ * @link http://www.naden.de/blog
+ */
+class CodeEditor_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public function activate()
+    {
+        Typecho_Plugin::factory('admin/theme-editor.php')->textarea = array('CodeEditor_Plugin', 'render');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public function deactivate(){}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public function config(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 插件实现方法
+     * 
+     * @access public
+     * @return void
+     */
+    public static function render()
+    {
+        $options = Typecho_Widget::widget('Widget_Options');
+        print( "\n<!-- Code Editor Plugin v1.1 http://www.naden.de/blog/wordpress-code-editor -->\n" );
+        /// die Variable brauchen wir, falls Wordpress in einem Unterverzeichnis installiert ist
+        printf( '<script type="text/javascript">var ce_url = \'%s\';</script>', Typecho_Common::url('CodeEditor', $options->pluginUrl) );
+        printf( '<script type="text/javascript" src="%s"></script>',
+        Typecho_Common::url('CodeEditor/code-editor.js', $options->pluginUrl));	 
+        print( "\n<!-- // Code Editor Plugin -->\n" );
+    }
+}
---
+++ usr/plugins/CodeEditor/code-editor.js
@@ -0,0 +1,40 @@
+/* <![CDATA[ */
+/*
+Plugin Name: Wordpress Code Editor
+Plugin URI: http://www.naden.de/blog/wordpress-code-editor
+Description: Javascript für das Wordpress Code Editor Plugin.
+Author: Naden Badalgogtapeh
+Version: 1.0
+Author URI: http://www.naden.de/blog
+*/
+( function() {
+/// hole form und textarea
+var ce_form = document.getElementById( 'theme' );
+var ce_textarea = document.getElementById( 'content' );
+if( ce_form && ce_textarea ) {
+	/// hooke ce_form.onsubmit
+	ce_form.onsubmit = function() {
+		newcontent.toggleEditor();
+		ce_form.submit();
+	}
+	/// dateitype erkennen
+	var ce_file = document.getElementsByName( 'file' );
+	var ce_file_type = 'php';
+	if( ce_file && ce_file.length > 0 ) {
+		if( ce_file[ 0 ].value.match( '\.css' ) ) {
+			ce_file_type = 'css';
+		}
+		else if( ce_file[ 0 ].value.match( '\.js' ) ) {
+			ce_file_type = 'js';
+		}
+		else if( ce_file[ 0 ].value.match( '\.html' ) ) {
+			ce_file_type = 'html';
+		}
+	}
+	/// setze Klassenname für codepress
+	ce_textarea.className = 'codepress ' + ce_file_type;
+	/// lade Editor
+	document.write( '<' + 'script type="text/javascript" src="' + window.ce_url + '/codepress/codepress.js"' + '>' + '<' + '/script' + '>' );
+}
+} )();
+/* ]]> */
