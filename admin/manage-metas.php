2c415788ee3d4a449500a382b452a06539a78463
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-12-20 13:48:08 +0000

    开始完善分类和标签管理 拖动完成

---
+++ admin/manage-metas.php
@@ -0,0 +1,64 @@
+<?php
+include 'common.php';
+include 'header.php';
+include 'menu.php';
+?>
+<div class="main">
+    <div class="body body-950">
+        <?php include 'page-title.php'; ?>
+        <div class="container typecho-page-main manage-metas">
+                <div class="column-18 start-01 tag-list">
+                    <?php Typecho_Widget::widget('Widget_Metas_Tag_Cloud', 'sort=mid&desc=0')->to($tags); ?>
+                    <h2><?php _e('标签列表'); ?></h2>
+                    <div class="items">
+                    <?php while($tags->next()): ?><span class="<?php $tags->split('size-1', 'size-2', 'size-3', 'size-4', 'size-5'); ?>"><?php $tags->name(); ?></span><?php endwhile; ?>
+                    </div>
+                </div>
+                <div class="column-06 start-19">
+                    <?php Typecho_Widget::widget('Widget_Metas_Category_List')->to($categories); ?>
+                    <ul class="category-list">
+                        <h2><?php _e('分类列表'); ?></h2>
+                        <li><a href="#"><?php $categories->name(); ?></a></li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'common-js.php'; ?>
+<script type="text/javascript">
+    /** 拖拽 */
+    $(document).getElements('.manage-metas .tag-list .items span').each(function (item) {
+        var top = item.getTop();
+        var left = item.getLeft();
+        
+        var typechoDrag = new Drag.Move(item, {
+            onSnap: function(el){
+                el.addClass('move');
+            },
+            onComplete: function(el){
+                el.removeClass('move');
+                el.setStyle('top', '');
+                el.setStyle('left', '');
+                el.setStyle('position', '');
+            }
+        });
+        item.setStyle('top', '');
+        item.setStyle('left', '');
+        item.setStyle('position', '');
+        
+        typechoDrag.addEvent('onBeforeStart', function () {
+            this.element.setStyle('position', 'absolute');
+            this.element.addClass('move');
+            
+            var _position = this.element.getCoordinates(this.element.getParent('.tag-list'));
+            
+            this.element.setStyle('top', _position.top);
+            this.element.setStyle('left', _position.left);
+        });
+    });
+    
+    var tagListItems = $(document).getElement('.manage-metas .tag-list .items');
+    tagListItems.setStyle('height', tagListItems.getStyle('height'));
+</script>
+<?php include 'copyright.php'; ?>
--- var/Widget/Metas/Tag/Cloud.php
+++ var/Widget/Metas/Tag/Cloud.php
@@ -20,48 +20,19 @@
 class Widget_Metas_Tag_Cloud extends Widget_Abstract_Metas
 {
     /**
-     * 分割数量
-     * 
-     * @access private
-     * @var integer
-     */
-    private $_split;
-    
-    /**
      * 入口函数
      * 
      * @access public
-     * @param string $sort 排序类型
-     * @param integer $split 分割数量
-     * @param boolean $ignoreZeroCount 忽略零数量
      * @return void
      */
-    public function __construct($sort = 'count', $split = 5, $ignoreZeroCount = false)
+    public function execute()
     {
-        parent::__construct();
-        
-        $this->_split = $split;
-        $select = $this->select()->where('`type` = ?', 'tag')->order('`' . $sort . '`', Typecho_Db::SORT_DESC);
-        
-        /** 过滤标题 */
-        if(empty(Typecho_Router::$current) &&
-        NULL != ($keywords = Typecho_Request::getParameter('keywords')) &&
-        Typecho_API::factory('Widget_Users_Current')->pass('editor', true))
-        {
-            $args = array();
-            $keywords = explode(' ', $keywords);
-            $args[] = implode(' OR ', array_fill(0, count($keywords), 'table.metas.`name` LIKE ?'));
-            
-            foreach($keywords as $keyword)
-            {
-                $args[] = '%' . Typecho_API::filterSearchQuery($keyword) . '%';
-            }
-            
-            call_user_func_array(array($select, 'where'), $args);
-        }
+        $this->parameter->setDefault(array('sort' => 'count', 'split' => 5, 'ignoreZeroCount' => false, 'desc' => true));
+        $select = $this->select()->where('type = ?', 'tag')->order($this->parameter->sort,
+        $this->parameter->desc ? Typecho_Db::SORT_DESC : Typecho_Db::SORT_ASC);
         
         /** 忽略零数量 */
-        if($ignoreZeroCount)
+        if($this->parameter->ignoreZeroCount)
         {
             $select->where('count > 0');
         }
@@ -80,7 +51,7 @@ class Widget_Metas_Tag_Cloud extends Widget_Abstract_Metas
     {
         $args = func_get_args();
         $num = func_num_args();
-        $size = min(intval($this->count / $this->_split), $num - 1);
+        $size = min(intval($this->count / $this->parameter->split), $num - 1);
         
         echo $args[$size];
     }
