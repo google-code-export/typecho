ddd4582e684908e8387e5b4a62dcaf55fde31b2b
fen1211 <fen1211@0318ff09-9246-0410-8118-259958648ee7>
2008-12-28 16:11:06 +0000

    统一模板为中文

--- usr/themes/new_default/comments.php
+++ usr/themes/new_default/comments.php
@@ -1,5 +1,5 @@
 		<div id="comments">
-			<h4><?php $this->commentsNum('No Response', 'One Response to"' . $this->title . '"', '%d Responses to "' . $this->title . '"'); ?></h4>
+			<h4><?php $this->commentsNum('当前暂无评论', '仅有一条评论', '已有 %d 条评论'); ?></h4>
 			<ol id="comment_list">
 			<?php $this->comments()->to($comments); ?>
             <?php while($comments->next()): ?>
@@ -13,17 +13,17 @@
 			</ol>
 
             <?php if($this->allow('comment')): ?>
-			<h4 id="response">Leave a Reply</h4>
+			<h4 id="response">添加新评论</h4>
 			<form method="post" action="<?php $this->commentUrl() ?>" id="comment_form">
                 <?php if($this->user->hasLogin()): ?>
                 <p>Logged in as <a href="<?php $this->options->adminUrl(); ?>"><?php $this->user->screenName(); ?></a>. <a href="<?php $this->options->index('Logout.do'); ?>" title="Logout">Logout &raquo;</a></p>
                 <?php else: ?>
-				<p><input type="text" name="author" class="text" size="35" value="<?php $this->remember('author'); ?>" /><label>Name (Required)</label></p>
-				<p><input type="text" name="mail" class="text" size="35" value="<?php $this->remember('mail'); ?>" /><label>E-mail (Required *will not be published)</label></p>
-				<p><input type="text" name="url" class="text" size="35" value="<?php $this->remember('url'); ?>" /><label>Website</label></p>
+				<p><input type="text" name="author" class="text" size="35" value="<?php $this->remember('author'); ?>" /><label>名字 *</label></p>
+				<p><input type="text" name="mail" class="text" size="35" value="<?php $this->remember('mail'); ?>" /><label>E-mail *</label></p>
+				<p><input type="text" name="url" class="text" size="35" value="<?php $this->remember('url'); ?>" /><label>网站 （选填）</label></p>
                 <?php endif; ?>
 				<p><textarea rows="10" cols="50" name="text"><?php $this->remember('text'); ?></textarea></p>
-				<p><input type="submit" value="Submit Comment" class="submit" /></p>
+				<p><input type="submit" value="提交评论" class="submit" /></p>
 			</form>
             <?php endif; ?>
 		</div>
