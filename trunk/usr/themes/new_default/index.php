babe684fcc964fc2e7c8d65222c80d3090188fa0
fen1211 <fen1211@0318ff09-9246-0410-8118-259958648ee7>
2008-12-29 03:11:58 +0000

    Issue 51: 使用i18n函数

--- trunk/usr/themes/new_default/index.php
+++ trunk/usr/themes/new_default/index.php
@@ -5,7 +5,7 @@
  * @package Typecho Default Theme 
  * @author fen
  * @version 1.0.5
- * @link http://hellowiki.com
+ * @link http://typecho.org
  */
  
  include('header.php');
@@ -15,7 +15,7 @@
 	<?php while($this->next()): ?>
         <div class="post">
 			<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-			<div class="entry_data">由 <?php $this->author(); ?> 在 <?php $this->date('F j, Y'); ?> 发布于 <?php $this->category(','); ?>. <?php $this->commentsNum('%d 条评论'); ?>.</div>
+			<div class="entry_data"><?php _e('作者：'); ?><?php $this->author(); ?> | <?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?> | <?php _e('分类：'); ?><?php $this->category(','); ?> | <?php $this->commentsNum('%d'); ?> <?php _e('条评论'); ?></div>
 			<div class="entry_text">
 				<?php $this->content('阅读剩余部分...'); ?>
 		    </div>
--- trunk/usr/themes/new_default/post.php
+++ trunk/usr/themes/new_default/post.php
@@ -3,14 +3,14 @@
     <div class="grid_10" id="content">
         <div class="post">
 			<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-			<div class="entry_data">由 <?php $this->author(); ?> 在 <?php $this->date('F j, Y'); ?> 发布于 <?php $this->category(','); ?>. <?php $this->commentsNum('%d 条评论'); ?>.</div>
+			<div class="entry_data"><?php _e('作者：'); ?><?php $this->author(); ?> | <?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?> | <?php _e('分类：'); ?><?php $this->category(','); ?></div>
 			<div class="entry_text">
 				<?php $this->content(); ?>
 		    </div>
 		</div>
 
 		<div class="clearfix" id="post_extra">
-			标签: <?php $this->tags(', ', true, 'none'); ?>
+			<?php _e('标签'); ?>: <?php $this->tags(', ', true, 'none'); ?>
 		</div>
 
 		<?php include('comments.php'); ?>
