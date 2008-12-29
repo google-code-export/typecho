fef0874e27f240fc24ed4ace9d73a1caaafd694b
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2008-12-29 11:00:22 +0000

    更新默认模板

---
+++ trunk/usr/themes/default/page.php
@@ -0,0 +1,15 @@
+<?php include('header.php'); ?>
+
+    <div class="grid_10" id="content">
+        <div class="post">
+			<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
+			<div class="entry_data"><?php _e('作者：'); ?><?php $this->author(); ?> | <?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?></div>
+			<div class="entry_text">
+				<?php $this->content('阅读剩余部分...'); ?>
+		    </div>
+		</div>
+
+		<?php include('comments.php'); ?>
+    </div><!-- end #content-->
+	<?php include('sidebar.php'); ?>
+	<?php include('footer.php'); ?>
