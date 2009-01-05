68fc079f3bbee16381d890b560e9f4df34081b56
sluke520 <sluke520@0318ff09-9246-0410-8118-259958648ee7>
2009-01-05 16:43:27 +0000

    WordpressToTypecho插件，未完成

---
+++ usr/plugins/WordpressToTypecho/Action.php
@@ -0,0 +1,207 @@
+<?php
+
+class WordpressToTypecho_Action extends Typecho_Widget implements Widget_Interface_Do
+{
+    public function doImport()
+    {
+        $options = $this->widget('Widget_Options');
+        $dbConfig = $options->plugin('WordpressToTypecho');
+
+        /** 初始化一个db */
+        if (Typecho_Db_Adapter_Mysql::isAvailable()) {
+            $db = new Typecho_Db('Mysql', $dbConfig->prefix);
+        } else {
+            $db = new Typecho_Db('Pdo_Mysql', $dbConfig->prefix);
+        }
+        
+        /** 只读即可 */
+        $db->addServer(array (
+          'host' => $dbConfig->host,
+          'user' => $dbConfig->user,
+          'password' => $dbConfig->password,
+          'charset' => 'utf8',
+          'port' => $dbConfig->port,
+          'database' => $dbConfig->database
+        ), Typecho_Db::READ);
+        
+        /** 删除当前内容 */
+        $masterDb = Typecho_Db::get();
+        $this->widget('Widget_Abstract_Contents')->to($contents)->delete($masterDb->sql()->where('1 = 1'));
+        $this->widget('Widget_Abstract_Comments')->to($comments)->delete($masterDb->sql()->where('1 = 1'));
+        $this->widget('Widget_Abstract_Metas')->to($metas)->delete($masterDb->sql()->where('1 = 1'));
+        $this->widget('Widget_Contents_Post_Edit')->to($edit);
+        $masterDb->query($masterDb->delete('table.relationships')->where('1 = 1'));
+        $userId = $this->widget('Widget_User')->uid;
+        
+        /** 转换全局变量 */
+		/** 
+        $rows = $db->fetchAll($db->select()->from('table.statics'));
+        $static = array();
+        foreach ($rows as $row) {
+            $static[$row['static_name']] = $row['static_value'];
+        }*/
+        
+        /** 转换文件 */
+        /**$files = $db->fetchAll($db->select()->from('table.files'));
+        if (!is_dir(__TYPECHO_ROOT_DIR__ . '/usr/uploads/')) {
+            mkdir(__TYPECHO_ROOT_DIR__ . '/usr/uploads/', 0766);
+        }
+        
+        $pattern = array();
+        $replace = array();
+        foreach ($files as $file) {
+            $path = __TYPECHO_ROOT_DIR__ . '/data/upload/' . substr($file['file_guid'], 0, 2) . '/' .
+            substr($file['file_guid'], 2, 2) . '/' . $file['file_guid'];
+            
+            if (is_file($path)) {
+                $file['file_time'] = empty($file['file_time']) ? $options->gmtTime : $file['file_time'];
+                $year = idate('Y', $file['file_time']);
+                $month = idate('m', $file['file_time']);
+                $day = idate('d', $file['file_time']);
+                
+                if (!is_dir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}")) {
+                    mkdir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}", 0766);
+                }
+                
+                if (!is_dir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}/{$month}")) {
+                    mkdir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}/{$month}", 0766);
+                }
+                
+                if (!is_dir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}/{$month}/{$day}")) {
+                    mkdir(__TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}/{$month}/{$day}", 0766);
+                }
+                
+                $parts = explode('.', $file['file_name']);
+                $ext = array_pop($parts);
+                copy($path, __TYPECHO_ROOT_DIR__ . "/usr/uploads/{$year}/{$month}/{$day}/{$file['file_id']}.{$ext}");
+                
+                $new = Typecho_Common::url("/usr/uploads/{$year}/{$month}/{$day}/{$file['file_id']}.{$ext}", $options->siteUrl);
+                $old = Typecho_Common::url("/res/{$file['file_id']}/{$file['file_name']}", $static['siteurl'] . '/index.php');
+                $pattern[] = '/' . str_replace('\/index\.php', '(\/index\.php)?', preg_quote($old, '/')) . '/is';
+                $replace[] = $new;
+            }
+        }
+        */
+        /** 转换评论 */
+        $i = 1;
+        
+        while (true) {
+            $result = $db->query($db->select()->from('table.comments')
+            ->order('comment_ID', Typecho_Db::SORT_ASC)->page($i, 100));
+            $j = 0;
+            
+            while ($row = $db->fetchRow($result)) {
+                $comments->insert(array(
+                    'coid'      =>  $row['comment_ID'],
+                    'cid'       =>  $row['comment_post_ID'],
+                    'created'   =>  $row['comment_date_gmt'],
+                    'author'    =>  $row['comment_user'],
+                    'authorId'  =>  $row['comment_author'],
+                    'ownerId'   =>  $row['comment_useid'],
+                    'mail'      =>  $row['comment_author_mail'],
+                    'url'       =>  $row['comment_author_url'],
+                    'ip'        =>  $row['comment_author_IP'],
+                    'agent'     =>  $row['comment_agent'],
+                    'text'      =>  $row['comment_content'],
+                    'type'      =>  $row['comment_type'],
+                    'status'    =>  $row['comment_approved'],
+                    'parent'    =>  $row['comment_parent']
+                ));
+                $j ++;
+                unset($row);
+            }
+            
+            if ($j < 100) {
+                break;
+            }
+            
+            $i ++;
+            unset($result);
+        }
+                
+		/** 转换Wordpress的terms表 */
+		$cats = $db->fetchAll($db->select()->from('table.terms'));
+        foreach ($cats as $cat) {
+            $metas->insert(array(
+                'mid'       	=>  $row['terms_id'],
+                'name'     		=>  $row['name'],
+                'slug'      	=>  $row['slug'],
+				'order'     	=>  '0',
+            ));
+        }
+		
+		/** 转换Wordpress的term_taxonomy表 */
+		$cats = $db->fetchAll($db->select()->from('table.term_taxonomy'));
+        foreach ($cats as $cat) {
+            $metas->insert(array(
+                'type'      	=>  $row['taxonomy'],
+                'description'   =>  $row['description'],
+                'count'      	=>  $row['count'],
+            ));
+        }
+		
+		/** 转换关系表 */
+		$cats = $db->fetchAll($db->select()->from('table.term_relationships'));
+        foreach ($cats as $cat) {
+            $relationships->insert(array(
+                'cid'      	=>  $row['object_id'],
+                'mid'   	=>  $row['term_taxonomy_id'],
+            ));
+        }
+		
+        /** 转换内容 */
+        $i = 1;
+        
+        while (true) {
+            $result = $db->query($db->select()->from('table.posts')
+            ->order('ID', Typecho_Db::SORT_ASC)->page($i, 100));
+            $j = 0;
+            
+            while ($row = $db->fetchRow($result)) {
+                $contents->insert(array(
+                    'cid'           =>  $row['ID'],
+                    'title'         =>  $row['post_title'],
+                    'slug'          =>  $row['post_name'],
+                    'created'       =>  $row['post_date_gmt'],
+                    'modified'      =>  $row['post_modified_gmt'],
+                    'text'          =>  preg_replace($pattern, $replace, $row['post_content']),
+                    'order'         =>  0,
+                    'authorId'      =>  $row['post_author'],
+                    'template'      =>  NULL,
+                    'type'          =>  $row['post_type'] ? 'page' : 'post',
+                    'status'        =>  $row['post_status'] ? 'draft' : 'publish',
+                    'password'      =>  $row['post_password'],
+                    'commentsNum'   =>  $row['comment_count'],
+                    'allowComment'  =>  $row['comment_status']? '0' : '1',
+                    'allowFeed'     =>  $row['post_allow_feed']? '0' : '1',
+                    'allowPing'     =>  $row['ping_status']? '0' : '1',
+                ));
+                
+                /** 插入分类关系 */
+                $edit->setCategories($row['ID'], array($row['terms_id']), !$row['post_status']);
+                
+                /** 设置标签 */
+                $edit->setTags($row['ID'], $row['name'], !$row['post_status']);
+                
+                $j ++;
+                unset($row);
+            }
+            
+            if ($j < 100) {
+                break;
+            }
+            
+            $i ++;
+            unset($result);
+        }
+        
+        $this->widget('Widget_Notice')->set(_t("数据已经转换完成"), NULL, 'success');
+        $this->response->goBack();
+    }
+
+    public function action()
+    {
+        $this->widget('Widget_User')->pass('administrator');
+        $this->onPost()->doImport();
+    }
+}
