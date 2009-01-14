b118b11f9e204e18c2ce17938d5b992085de3aee
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-01-14 08:11:46 +0000

    增加伪类支持

--- tools/build.css.php
+++ tools/build.css.php
@@ -16,8 +16,20 @@ if (!is_file($file)) {
     exit;
 }
 
+$oldSize = filesize($file);
 $lines = file_get_contents($file);
 $result = array();
+$hacks = array();
+
+function rememberHacks($matches)
+{
+    global $hacks;
+    $hacks[] = trim($matches[1]) . '{' . trim($matches[2]) . '{' . trim($matches[3]) . '}}';
+    return NULL;
+}
+
+$lines = preg_replace_callback("/([\#_a-z0-9-\*\.\:\ \+\>\,\(\)\@]+)\s*\{\s*([\#_a-z0-9-\*\.\:\ \+\>\,]+)\s*\{(.*?)\}\s*\}/is", 'rememberHacks', $lines);
+
 
 /** 去掉注释 */
 $lines = preg_replace("/\/\*(.*?)\*\//is", '', $lines);
@@ -30,7 +42,7 @@ if (preg_match_all("/([\#_a-z0-9-\*\.\:\ \+\>\,]+)\s*\{(.*?)\}/is", $lines, $mat
             list ($name, $property) = array_map('trim', explode(':', $value));
             
             foreach ($items as $item) {
-                $result[$name . ': ' . $property][] = $item;
+                $result[$name . ':' . $property][] = $item;
             }
         }
     }
@@ -47,5 +59,7 @@ foreach ($final as $key => $val) {
     $string .= $key . '{' . implode(';', $val) . '}';
 }
 
+$string .= implode('', $hacks);
 file_put_contents($file, $string);
-echo 'compiled: ' . $file . "\n";
+$newSize = filesize($file);
+echo 'compiled: ' . $file . " {$oldSize} -> {$newSize}\n";
