57350bdd62b304265f1ebd97fd528ecf8a23762e
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-02-17 07:40:05 +0000

    增加调整编辑器大小功能

--- var/Typecho/Common.php
+++ var/Typecho/Common.php
@@ -29,7 +29,7 @@ class Typecho_Common
     const ELEMENT_HTML_TAG = 'div|blockquote|pre|td|li';
     
     /** 程序版本 */
-    const VERSION = '0.4/9.2.3';
+    const VERSION = '0.5/9.2.17';
     
     /**
      * 缓存的包含路径
