d3566b08df19d507301240df5512d1b29e540f40
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-03-10 06:34:10 +0000

    升级make

---
+++ trunk/tools/mcecp.php
@@ -0,0 +1,22 @@
+<?php
+
+/** 参数不存在则退出 */
+if (!isset($argv[1])) {
+    echo 'no args';
+    exit(1);
+}
+
+/** 解析所有的参数 */
+parse_str($argv[1], $options);
+
+/** 必要参数检测 */
+if (!isset($options['in']) || !isset($options['out'])) {
+    echo 'no input or output file';
+    exit(1);
+}
+
+if (file_exists($options['out'])) {
+    copy($options['in'], str_replace('jscripts/tiny_mce/', '', $options['out']));
+}
+
+exit(0);
