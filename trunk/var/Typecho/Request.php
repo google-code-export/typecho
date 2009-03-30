9034198802009f0f28240b0d1becc7e9fc2c1e3a
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-03-30 01:54:05 +0000

    使用zend framework部分代码加强rewrite跨服务器兼容性，目前已经兼容cgi和iis服务器

--- trunk/var/Typecho/Request.php
+++ trunk/var/Typecho/Request.php
@@ -22,6 +22,14 @@ class Typecho_Request
      * @var array
      */
     private static $_params = array();
+    
+    /**
+     * 路径信息
+     * 
+     * @access private
+     * @var string
+     */
+    private static $_pathInfo = NULL;
 
     /**
      * 获取指定的http传递参数
@@ -187,40 +195,100 @@ class Typecho_Request
      */
     public static function getPathInfo()
     {
-        if (!empty($_SERVER['PATH_INFO']) || NULL != getenv('PATH_INFO')) {
-            $pathInfo = empty($_SERVER['PATH_INFO']) ? getenv('PATH_INFO') : $_SERVER['PATH_INFO'];
-            if (0 === strpos($pathInfo,$_SERVER['SCRIPT_NAME'])) {
-                $path = substr($pathInfo, strlen($_SERVER['SCRIPT_NAME']));
-            } else {
-                $path = $pathInfo;
+        /** 缓存信息 */
+        if (NULL !== self::$_pathInfo) {
+            return self::$_pathInfo;
+        }
+    
+        //参考Zend Framework对pahtinfo的处理, 更好的兼容性
+        $pathInfo = NULL;
+        
+        //处理requestUri
+        $requestUri = NULL;
+        
+        if (isset($_SERVER['HTTP_X_REWRITE_URL'])) { // check this first so IIS will catch
+            $requestUri = $_SERVER['HTTP_X_REWRITE_URL'];
+        } elseif (isset($_SERVER['REQUEST_URI'])) {
+            $requestUri = $_SERVER['REQUEST_URI'];
+            if (isset($_SERVER['HTTP_HOST']) && strstr($requestUri, $_SERVER['HTTP_HOST'])) {
+                $pathInfo    = parse_url($requestUri, PHP_URL_PATH);
+                $queryString = parse_url($requestUri, PHP_URL_QUERY);
+                $requestUri  = $pathInfo
+                             . ((empty($queryString)) ? '' : '?' . $queryString);
             }
-        } else if (!empty($_SERVER['ORIG_PATH_INFO']) || NULL != getenv('ORIG_PATH_INFO')) {
-            $pathInfo = empty($_SERVER['ORIG_PATH_INFO']) ? getenv('ORIG_PATH_INFO') : $_SERVER['ORIG_PATH_INFO'];
-            if (0 === strpos($pathInfo, $_SERVER['SCRIPT_NAME']) && 0 === strpos($pathInfo, $_SERVER['SCRIPT_NAME'])) {
-                $path = substr($pathInfo, strlen($_SERVER['SCRIPT_NAME']));
-            } else {
-                $path = $pathInfo;
+        } elseif (isset($_SERVER['ORIG_PATH_INFO'])) { // IIS 5.0, PHP as CGI
+            $requestUri = $_SERVER['ORIG_PATH_INFO'];
+            if (!empty($_SERVER['QUERY_STRING'])) {
+                $requestUri .= '?' . $_SERVER['QUERY_STRING'];
             }
-        } else if (!empty($_SERVER["REDIRECT_URL"])) {
-            $path = $_SERVER["REDIRECT_URL"];
+        } else {
+            return self::$_pathInfo = '/';
+        }
+        
+        //处理baseUrl
+        $filename = (isset($_SERVER['SCRIPT_FILENAME'])) ? basename($_SERVER['SCRIPT_FILENAME']) : '';
 
-            if (empty($_SERVER['QUERY_STRING']) || $_SERVER['QUERY_STRING'] == $_SERVER["REDIRECT_QUERY_STRING"]) {
-                $parsedUrl = parse_url($_SERVER["REQUEST_URI"]);
-                if (!empty($parsedUrl['query'])) {
-                    $_SERVER['QUERY_STRING'] = $parsedUrl['query'];
-                    parse_str($parsedUrl['query'], $GET);
-                    $_GET = array_merge($_GET, $GET);
-
-                    reset($_GET);
-                } else {
-                    unset($_SERVER['QUERY_STRING']);
-                }
-
-                reset($_SERVER);
-            }
+        if (isset($_SERVER['SCRIPT_NAME']) && basename($_SERVER['SCRIPT_NAME']) === $filename) {
+            $baseUrl = $_SERVER['SCRIPT_NAME'];
+        } elseif (isset($_SERVER['PHP_SELF']) && basename($_SERVER['PHP_SELF']) === $filename) {
+            $baseUrl = $_SERVER['PHP_SELF'];
+        } elseif (isset($_SERVER['ORIG_SCRIPT_NAME']) && basename($_SERVER['ORIG_SCRIPT_NAME']) === $filename) {
+            $baseUrl = $_SERVER['ORIG_SCRIPT_NAME']; // 1and1 shared hosting compatibility
+        } else {
+            // Backtrack up the script_filename to find the portion matching
+            // php_self
+            $path    = isset($_SERVER['PHP_SELF']) ? $_SERVER['PHP_SELF'] : '';
+            $file    = isset($_SERVER['SCRIPT_FILENAME']) ? $_SERVER['SCRIPT_FILENAME'] : '';
+            $segs    = explode('/', trim($file, '/'));
+            $segs    = array_reverse($segs);
+            $index   = 0;
+            $last    = count($segs);
+            $baseUrl = '';
+            do {
+                $seg     = $segs[$index];
+                $baseUrl = '/' . $seg . $baseUrl;
+                ++$index;
+            } while (($last > $index) && (false !== ($pos = strpos($path, $baseUrl))) && (0 != $pos));
+        }
+        
+        // Does the baseUrl have anything in common with the request_uri?
+        $finalBaseUrl = NULL;
+        
+        if (0 === strpos($requestUri, $baseUrl)) {
+            // full $baseUrl matches
+            $finalBaseUrl = $baseUrl;
+        } else if (0 === strpos($requestUri, dirname($baseUrl))) {
+            // directory portion of $baseUrl matches
+            $finalBaseUrl = rtrim(dirname($baseUrl), '/');
+        } else if (!strpos($requestUri, basename($baseUrl))) {
+            // no match whatsoever; set it blank
+            $finalBaseUrl = '';
+        } else if ((strlen($requestUri) >= strlen($baseUrl))
+            && ((false !== ($pos = strpos($requestUri, $baseUrl))) && ($pos !== 0)))
+        {
+            // If using mod_rewrite or ISAPI_Rewrite strip the script filename
+            // out of baseUrl. $pos !== 0 makes sure it is not matching a value
+            // from PATH_INFO or QUERY_STRING
+            $baseUrl = substr($requestUri, 0, $pos + strlen($baseUrl));
+        }
+        
+        $finalBaseUrl = (NULL === $finalBaseUrl) ? rtrim($baseUrl, '/') : $finalBaseUrl;
+        
+        // Remove the query string from REQUEST_URI
+        if ($pos = strpos($requestUri, '?')) {
+            $requestUri = substr($requestUri, 0, $pos);
+        }
+        
+        if ((null !== $finalBaseUrl)
+            && (false === ($pathInfo = substr($requestUri, strlen($finalBaseUrl)))))
+        {
+            // If substr() returns false then PATH_INFO is set to an empty string
+            $pathInfo = '/';
+        } elseif (null === $finalBaseUrl) {
+            $pathInfo = $requestUri;
         }
 
-        return empty($path) ? '/' : $path;
+        return (self::$_pathInfo = (empty($pathInfo) ? '/' : $pathInfo));
     }
 
     /**
