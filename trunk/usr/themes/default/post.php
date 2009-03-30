097943f3525c4307f1c97f1c75cbf5ab4ec82b62
fen1211 <fen1211@0318ff09-9246-0410-8118-259958648ee7>
2009-03-30 08:00:16 +0000

    继续调整结构

--- trunk/usr/themes/default/post.php
+++ trunk/usr/themes/default/post.php
@@ -3,15 +3,13 @@
     <div class="grid_10" id="content">
         <div class="post">
 			<h2 class="entry_title"><a href="<?php $this->permalink() ?>"><?php $this->title() ?></a></h2>
-			<div class="entry_data">
+			<p class="entry_data">
 				<span><?php _e('作者：'); ?><?php $this->author(); ?></span>
 				<span><?php _e('发布时间：'); ?><?php $this->date('F j, Y'); ?></span>
-				<span class="no-border"><?php _e('分类：'); ?><?php $this->category(','); ?></span>
-			</div>
-			<div class="entry_text">
-				<?php $this->content(); ?>
-				<p><?php _e('标签'); ?>: <?php $this->tags(', ', true, 'none'); ?></p>
-		    </div>
+				<?php _e('分类：'); ?><?php $this->category(','); ?>
+			</p>
+			<?php $this->content(); ?>
+			<p class="tags"><?php _e('标签'); ?>: <?php $this->tags(', ', true, 'none'); ?></p>
 		</div>
 
 		<?php include('comments.php'); ?>
