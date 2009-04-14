d55258ffb6161590d23faecb0bfc5d7964c76fcb
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-04-14 11:02:55 +0000

    初步整合

--- trunk/admin/manage-medias.php
+++ trunk/admin/manage-medias.php
@@ -8,8 +8,12 @@ include 'menu.php';
     <div class="body body-950">
         <?php include 'page-title.php'; ?>
         <div class="container typecho-page-main">
-            <div class="column-22 start-02">
-                <?php Typecho_Widget::widget('Widget_Options_Reading')->form()->render(); ?>
+            <div class="column-16 start-01">
+                <ul class="typecho-list-notable clearfix">
+                </ul>
+            </div>
+            <div class="column-08 start-17 typecho-mini-panel typecho-radius-topleft typecho-radius-topright typecho-radius-bottomleft typecho-radius-bottomright">
+            asdf
             </div>
         </div>
     </div>
