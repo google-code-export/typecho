14d8430d3100cdaab423c0022029f7b63f97ef6a
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-05-04 03:23:45 +0000

    更新文件权限

--- var/Widget/Upload.php
+++ var/Widget/Upload.php
@@ -36,7 +36,7 @@ class Widget_Upload extends Widget_Abstract_Contents implements Widget_Interface
         
         //创建上传目录
         if (!is_dir($path)) {
-            if (!@mkdir($path, 0644)) {
+            if (!@mkdir($path, 0777)) {
                 return false;
             }
         }
@@ -50,21 +50,21 @@ class Widget_Upload extends Widget_Abstract_Contents implements Widget_Interface
         
         //创建年份目录
         if (!is_dir($path = $path . '/' . $date->year)) {
-            if (!@mkdir($path, 0644)) {
+            if (!@mkdir($path, 0777)) {
                 return false;
             }
         }
         
         //创建月份目录
         if (!is_dir($path = $path . '/' . $date->month)) {
-            if (!@mkdir($path, 0644)) {
+            if (!@mkdir($path, 0777)) {
                 return false;
             }
         }
         
         //创建日期目录
         if (!is_dir($path = $path . '/' . $date->day)) {
-            if (!@mkdir($path, 0644)) {
+            if (!@mkdir($path, 0777)) {
                 return false;
             }
         }
