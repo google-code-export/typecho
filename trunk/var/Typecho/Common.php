01065e8781e5bab201ebf9efcf6f79fc616a11cf
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-05-07 08:16:40 +0000

    修正上传接口，增加mime 增加xmlrpc上传接口

--- trunk/var/Typecho/Common.php
+++ trunk/var/Typecho/Common.php
@@ -909,4 +909,92 @@ EOF;
         $path = (0 === strpos($path, './')) ? substr($path, 2) : $path;
         return rtrim($prefix, '/') . '/' . str_replace('//', '/', ltrim($path, '/'));
     }
+    
+    /**
+     * 获取图片
+     * 
+     * @access public
+     * @param string $fileName 文件名
+     * @return string
+     */
+    public static function mimeContentType($fileName)
+    {
+        if (function_exists('mime_content_type')) {
+            return mime_content_type($fileName);
+        } else if (function_exists('finfo_open')) {
+            $fInfo = finfo_open(FILEINFO_MIME);
+            $mimeType = finfo_file($fInfo, $fileName);
+            finfo_close($fInfo);
+            return $mimetype;
+        } else {
+            $mimeTypes = array(
+                'txt' => 'text/plain',
+                'htm' => 'text/html',
+                'html' => 'text/html',
+                'php' => 'text/html',
+                'jsp' => 'text/html',
+                'asp' => 'text/html',
+                'css' => 'text/css',
+                'js' => 'application/javascript',
+                'json' => 'application/json',
+                'xml' => 'application/xml',
+                'swf' => 'application/x-shockwave-flash',
+                'flv' => 'video/x-flv',
+
+                // images
+                'png' => 'image/png',
+                'jpe' => 'image/jpeg',
+                'jpeg' => 'image/jpeg',
+                'jpg' => 'image/jpeg',
+                'gif' => 'image/gif',
+                'bmp' => 'image/bmp',
+                'ico' => 'image/vnd.microsoft.icon',
+                'tiff' => 'image/tiff',
+                'tif' => 'image/tiff',
+                'svg' => 'image/svg+xml',
+                'svgz' => 'image/svg+xml',
+
+                // archives
+                'zip' => 'application/zip',
+                'rar' => 'application/x-rar-compressed',
+                'exe' => 'application/x-msdownload',
+                'msi' => 'application/x-msdownload',
+                'cab' => 'application/vnd.ms-cab-compressed',
+
+                // audio/video
+                'mp3' => 'audio/mpeg',
+                'qt' => 'video/quicktime',
+                'mov' => 'video/quicktime',
+
+                // adobe
+                'pdf' => 'application/pdf',
+                'psd' => 'image/vnd.adobe.photoshop',
+                'ai' => 'application/postscript',
+                'eps' => 'application/postscript',
+                'ps' => 'application/postscript',
+
+                // ms office
+                'doc' => 'application/msword',
+                'rtf' => 'application/rtf',
+                'xls' => 'application/vnd.ms-excel',
+                'ppt' => 'application/vnd.ms-powerpoint',
+
+                // open office
+                'odt' => 'application/vnd.oasis.opendocument.text',
+                'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+            );
+            
+            $part = explode('.', $fileName);
+            $size = count($part);
+            
+            if ($size > 1) {
+                $ext = $part[$size - 1];
+                if (isset($mimeTypes[$ext])) {
+                    return $mimeTypes[$ext];
+                }
+            }
+            
+            return 'application/octet-stream';
+        }
+    }
 }
