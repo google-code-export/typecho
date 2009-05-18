eefab776ea940453a20b89662916fca2b4ab1e98
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-05-18 08:22:37 +0000

    修正语法错误

--- admin/option-plugin.php
+++ admin/option-plugin.php
@@ -9,9 +9,7 @@ include 'menu.php';
         <?php include 'page-title.php'; ?>
         <div class="container typecho-page-main">
             <div class="column-22 start-02">
-<?php endif; ?>
                 <?php Typecho_Widget::widget('Widget_Plugins_Config')->config()->render(); ?>
-<?php if (!Typecho_Request::isAjax()): ?>
             </div>
         </div>
     </div>
