72a2dcddaf3ea42066630da01cfdae9dc7b35b1f
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-05-19 05:48:31 +0000

    附件管理完成

--- trunk/admin/media.php
+++ trunk/admin/media.php
@@ -2,6 +2,8 @@
 include 'common.php';
 include 'header.php';
 include 'menu.php';
+
+Typecho_Widget::widget('Widget_Contents_Attachment_Edit')->to($attachment);
 ?>
 
 <div class="main">
@@ -9,11 +11,22 @@ include 'menu.php';
         <?php include 'page-title.php'; ?>
         <div class="container typecho-page-main">
             <div class="column-16 start-01">
-                <ul class="typecho-list-notable clearfix">
-                </ul>
+                <div class="typecho-attachment-photo-box">
+                    <?php if ($attachment->attachment->isImage): ?>
+                    <img src="<?php $attachment->attachment->url(); ?>" alt="<?php $attachment->attachment->name(); ?>" />
+                    <?php endif; ?>
+                    
+                    <div class="description">
+                        <ul>
+                            <?php $mime = Typecho_Common::mimeIconType($attachment->attachment->mime); ?>
+                            <li><span class="typecho-mime typecho-mime-<?php echo $mime; ?>"></span><strong><?php $attachment->attachment->name(); ?></strong> <small><?php echo number_format(ceil($attachment->attachment->size / 1024)); ?> Kb</small></li>
+                            <li><input type="text" readonly class="text" value="<?php $attachment->attachment->url(); ?>" /></li>
+                        </ul>
+                    </div>
+                </div>
             </div>
             <div class="column-08 start-17 typecho-mini-panel typecho-radius-topleft typecho-radius-topright typecho-radius-bottomleft typecho-radius-bottomright">
-            asdf
+                <?php $attachment->form()->render(); ?>
             </div>
         </div>
     </div>
