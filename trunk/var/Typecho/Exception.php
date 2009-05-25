d3532d7b9e89b9aa94c337c02f4f92a84c744753
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-05-25 07:38:05 +0000

    增加登录延时 改变安装程序策略,不再使用默认密码12345,而使用随机密码 修正安装程序对异常类型的判断

--- trunk/var/Typecho/Exception.php
+++ trunk/var/Typecho/Exception.php
@@ -14,4 +14,11 @@
  * @package Exception
  */
 class Typecho_Exception extends Exception
-{}
+{
+
+    public function __construct($message, $code = 0)
+    {
+        $this->message = $message;
+        $this->code = $code;
+    }
+}
