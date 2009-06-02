5e2cce480527115c9231cb6d1c6852769473696e
fen1211 <fen1211@0318ff09-9246-0410-8118-259958648ee7>
2009-06-02 09:12:51 +0000

    修改默认风格样式。

--- usr/themes/default/sidebar.php
+++ usr/themes/default/sidebar.php
@@ -1,33 +1,40 @@
 
     <div class="grid_4" id="sidebar">
-    
-	    <div id="search" class="widget">
-			<h3><?php _e('站内搜索'); ?></h3>
-	        <form method="post" action="">
-            	<div><input type="text" name="s" class="text" size="20" /> <input type="submit" class="submit" value="搜索" /></div>
-	        </form>
-	    </div>
 
 	    <div class="widget">
-			<h3><?php _e('最新文章'); ?></h3>
+			<h3 class="alt"><?php _e('最新文章'); ?></h3 class="alt">
             <ul>
                 <?php $this->widget('Widget_Contents_Post_Recent')
                 ->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
             </ul>
 	    </div>
+
+	    <div id="search" class="widget">
+			<h3 class="alt"><?php _e('站内搜索'); ?></h3 class="alt">
+	        <form method="post" action="">
+            	<div><input type="text" name="s" class="text" size="20" /> <input type="submit" class="submit" value="搜索" /></div>
+	        </form>
+	    </div>
+    
+        <div class="widget">
+			<h3 class="alt"><?php _e('页面'); ?></h3 class="alt">
+            <ul>	 
+				<?php $this->widget('Widget_Contents_Page_List')->parse('<li><a href="{permalink}">{title}</a></li>'); ?>
+            </ul>
+		</div>
     
 	    <div class="widget">
-			<h3><?php _e('最近回复'); ?></h3>
+			<h3 class="alt"><?php _e('最近回复'); ?></h3 class="alt">
             <ul>
             <?php $this->widget('Widget_Comments_Recent')->to($comments); ?>
             <?php while($comments->next()): ?>
-                <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?></a>: <?php $comments->excerpt(28, '...'); ?></li>
+                <li><a href="<?php $comments->permalink(); ?>"><?php $comments->author(false); ?></a>: <?php $comments->excerpt(50, '...'); ?></li>
             <?php endwhile; ?>
             </ul>
 	    </div>
 
         <div class="widget">
-			<h3><?php _e('分类'); ?></h3>
+			<h3 class="alt"><?php _e('分类'); ?></h3 class="alt">
             <ul>
                 <?php $this->widget('Widget_Metas_Category_List')
                 ->parse('<li><a href="{permalink}">{name}</a> ({count})</li>'); ?>
@@ -35,7 +42,7 @@
 		</div>
 
         <div class="widget">
-			<h3><?php _e('归档'); ?></h3>
+			<h3 class="alt"><?php _e('归档'); ?></h3 class="alt">
             <ul>
                 <?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
                 ->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
@@ -43,7 +50,7 @@
 		</div>
 
 		<div class="widget">
-			<h3><?php _e('其它'); ?></h3>
+			<h3 class="alt"><?php _e('其它'); ?></h3 class="alt">
             <ul>
                 <?php if($this->user->hasLogin()): ?>
 					<li class="last"><a href="<?php $this->options->adminUrl(); ?>"><?php _e('进入后台'); ?> (<?php $this->user->screenName(); ?>)</a></li>
