87b38c28d03cf613a9f9e5d0baf394a7828e558f
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-08-19 01:19:13 +0000

    fix issue 293

--- trunk/var/Typecho/Common.php
+++ trunk/var/Typecho/Common.php
@@ -197,9 +197,14 @@ class Typecho_Common
      */
     public static function exceptionHandle(Exception $exception)
     {
+        $obHandles = ob_list_handlers();
+        
         @ob_end_clean();
-        ob_start();
-        header('Content-Encoding: ', true);
+        if (in_array('ob_gzhandler', $obHandles)) {
+            ob_start('ob_gzhandler');
+        } else {
+            ob_start();
+        }
     
         if (!self::$config['exception']) {
             //@ob_clean();
