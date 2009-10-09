2350049d055aa1d2163238890a70422cff11c608
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-10-09 10:45:36 +0000

    修正pgsql兼容性

--- var/Widget/Contents/Related.php
+++ var/Widget/Contents/Related.php
@@ -21,6 +21,20 @@
 class Widget_Contents_Related extends Widget_Abstract_Contents
 {
     /**
+     * 获取查询对象
+     * 
+     * @access public
+     * @return Typecho_Db_Query
+     */
+    public function select()
+    {
+        return $this->db->select('DISTINCT table.contents.cid', 'table.contents.title', 'table.contents.slug', 'table.contents.created', 'table.contents.authorId',
+        'table.contents.modified', 'table.contents.type', 'table.contents.status', 'table.contents.text', 'table.contents.commentsNum', 'table.contents.order',
+        'table.contents.template', 'table.contents.password', 'table.contents.allowComment', 'table.contents.allowPing', 'table.contents.allowFeed')
+        ->from('table.contents');
+    }
+
+    /**
      * 执行函数,初始化数据
      * 
      * @access public
@@ -41,7 +55,6 @@ class Widget_Contents_Related extends Widget_Abstract_Contents
             ->where('table.contents.created < ?', $this->options->gmtTime)
             ->where('table.contents.type = ?', $this->parameter->type)
             ->order('table.contents.created', Typecho_Db::SORT_DESC)
-            ->group('table.contents.cid')
             ->limit($this->parameter->limit), array($this, 'push'));
         }
     }
