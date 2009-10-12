06f8725f8915efb1741f8274e6526a10a290d71a
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-10-12 07:42:02 +0000

    修正plugin地址

--- trunk/admin/common.php
+++ trunk/admin/common.php
@@ -9,11 +9,11 @@ if (!@include_once __DIR__ . '/../config.inc.php') {
     exit;
 }
 
-/** 注册一个初始化插件 */
-Typecho_Plugin::factory('admin/common.php')->begin();
-
 /** 初始化组件 */
 Typecho_Widget::widget('Widget_Init');
+
+/** 注册一个初始化插件 */
+Typecho_Plugin::factory('admin/common.php')->begin();
 
 Typecho_Widget::widget('Widget_Options')->to($options);
 Typecho_Widget::widget('Widget_User')->to($user);
