2bd107c8c0bc4115050e9983d729d5b82b20a839
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-10-16 07:13:51 +0000

    去掉对来源页的判断

--- var/Widget/Feedback.php
+++ var/Widget/Feedback.php
@@ -256,15 +256,7 @@ class Widget_Feedback extends Widget_Abstract_Comments implements Widget_Interfa
         $this->widget('Widget_Archive', NULL, $params)->to($this->_content)->have() && 
         $this->_content->is('single') && 
         in_array($callback, array('comment', 'trackback'))) {
-            /** 判断来源 */
-            // ~ fix Issue 38
-            if ('comment' == $callback && 0 !== strpos($_SERVER['HTTP_REFERER'], $this->_content->permalink)) {
-                //增加对自定义首页的判断
-                if ($this->options->customHomePage != $this->_content->cid) {
-                    throw new Typecho_Widget_Exception(_t('来源页不合法'), 403);
-                }
-            }
-            
+        
             /** 如果文章不允许反馈 */
             if ('comment' == $callback && !$this->_content->allow('comment')) {
                 throw new Typecho_Widget_Exception(_t('对不起,此内容的反馈被禁止.'), 403);
