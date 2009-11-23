8509326e214fc4695af4cf042b948a84aa7e3f3b
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2009-11-23 01:43:40 +0000

    增加session初始化 修正升级版本后仍出现升级提示的bug

--- var/Widget/Init.php
+++ var/Widget/Init.php
@@ -25,6 +25,9 @@ class Widget_Init extends Typecho_Widget
         /** 对变量赋值 */
         $options = $this->widget('Widget_Options');
         
+        /** 开始会话 */
+        @session_start();
+        
         /** 初始化charset */
         Typecho_Common::$config['charset'] = $options->charset;
         
