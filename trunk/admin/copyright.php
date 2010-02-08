0d4f27d4e546c16674ebbb3fd8ecbdffcd02663a
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2010-02-08 06:15:44 +0000

    更加强大的标签检查器

--- trunk/admin/copyright.php
+++ trunk/admin/copyright.php
@@ -4,10 +4,10 @@
     <div class="copyright"><?php _e('基于 <em>%s %s</em> <small> | %s</small> 构建', $soft, $prefixVersion, $suffixVersion); ?></div>
     <div class="resource">
         <ul>
-            <li><a href="http://typecho.org"><?php _e('文档'); ?></a></li>
+            <li><a href="http://docs.typecho.org"><?php _e('文档'); ?></a></li>
             <li><a href="http://forum.typecho.org"><?php _e('支持论坛'); ?></a></li>
             <li><a href="http://code.google.com/p/typecho/issues/entry"><?php _e('报告错误'); ?></a></li>
-            <li><a href="#"><?php _e('其他资源'); ?></a></li>
+            <li><a href="http://extends.typecho.org"><?php _e('其他资源'); ?></a></li>
         </ul>
     </div>
 </div>
