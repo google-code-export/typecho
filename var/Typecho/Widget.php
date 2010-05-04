352b36dbb0759fe9112c4c0cbc6a2c8ed127f1a0
magike.net <magike.net@0318ff09-9246-0410-8118-259958648ee7>
2010-05-04 04:19:22 +0000

    fix issue 438 修正google code代码高亮插件css冲突

--- var/Typecho/Widget.php
+++ var/Typecho/Widget.php
@@ -338,7 +338,10 @@ abstract class Typecho_Widget
 
         if (!$this->row) {
             reset($this->stack);
-            $this->row = $this->stack[key($this->stack)];
+            if ($this->stack) {
+                $this->row = $this->stack[key($this->stack)];
+            }
+            
             $this->sequence = 0;
             return false;
         }
