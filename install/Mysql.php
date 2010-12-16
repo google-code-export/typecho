9f4638091aad7631a8d4cbfc8b3f7e7aba95aa27
jianxieshui@gmail.com <jianxieshui@gmail.com@0318ff09-9246-0410-8118-259958648ee7>
2010-12-16 11:53:45 +0000

    提交对Sina App Engine的兼容性判断

--- install/Mysql.php
+++ install/Mysql.php
@@ -1,4 +1,33 @@
 <?php if(!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+<?php if (defined('SAE_ACCESSKEY') && defined('SAE_SECRETKEY')): ?>
+<?php //这里是专门为Sina App Engine做的判断 ?>
+<li>
+<label class="typecho-label"><?php _e('数据库地址'); ?></label>
+<input type="text" class="text" name="dbHost" value="<?php _v('dbHost', 'm'.$_SERVER['HTTP_MYSQLPORT'].'.mysql.sae.sina.com.cn'); ?>" />
+<p class="description"><?php _e('这里Sina App Engine自动分配的数据库地址，请保留默认设置'); ?></p>
+</li>
+<li>
+<label class="typecho-label"><?php _e('数据库端口'); ?></label>
+<input type="text" class="text" name="dbPort" value="<?php _v('dbPort', $_SERVER['HTTP_MYSQLPORT']); ?>" />
+<p class="description"><?php _e('如果您不知道此选项的意义, 请保留默认设置'); ?></p>
+</li>
+<li>
+<label class="typecho-label"><?php _e('数据库用户名'); ?></label>
+<input type="text" class="text" name="dbUser" value="<?php _v('dbUser', SAE_ACCESSKEY); ?>" />
+<p class="description"><?php _e('这里Sina App Engine自动分配的用户名，请保留默认设置'); ?></p>
+</li>
+<li>
+<label class="typecho-label"><?php _e('数据库密码'); ?></label>
+<input type="password" class="text" name="dbPassword" value="<?php _v('dbPassword', SAE_SECRETKEY); ?>" />
+</li>
+<li>
+<label class="typecho-label"><?php _e('数据库名'); ?></label>
+<input type="text" class="text" name="dbDatabase" value="<?php _v('dbDatabase', 'app_'.$_SERVER['HTTP_APPNAME']); ?>" />
+<p class="description"><?php _e('请您指定数据库名称'); ?></p>
+</li>
+<?php //结束 ?>
+<?php  else: ?>
 <li>
 <label class="typecho-label"><?php _e('数据库地址'); ?></label>
 <input type="text" class="text" name="dbHost" value="<?php _v('dbHost', 'localhost'); ?>"/>
@@ -23,4 +52,7 @@
 <input type="text" class="text" name="dbDatabase" value="<?php _v('dbDatabase', 'typecho'); ?>" />
 <p class="description"><?php _e('请您指定数据库名称'); ?></p>
 </li>
+<?php  endif; ?>
+
+
 <input type="hidden" name="dbCharset" value="<?php _e('utf8'); ?>" />
