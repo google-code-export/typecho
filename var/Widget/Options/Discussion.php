8a9fb7c6bb8084de8039175a8cd21825ac53558f
cnxuchao@gmail.com <cnxuchao@gmail.com@0318ff09-9246-0410-8118-259958648ee7>
2011-02-21 04:37:39 +0000

    修正错别字

--- var/Widget/Options/Discussion.php
+++ var/Widget/Options/Discussion.php
@@ -40,7 +40,7 @@ class Widget_Options_Discussion extends Widget_Abstract_Options implements Widge
 
         /** 评论列表数目 */
         $commentsListSize = new Typecho_Widget_Helper_Form_Element_Text('commentsListSize', NULL, $this->options->commentsListSize,
-        _t('评论列表数目'), _t('此数目用于指定显示在侧边拦中的评论列表数目.'));
+        _t('评论列表数目'), _t('此数目用于指定显示在侧边栏中的评论列表数目.'));
         $commentsListSize->input->setAttribute('class', 'mini');
         $form->addInput($commentsListSize->addRule('isInteger', _t('请填入一个数字')));
 
