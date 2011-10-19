53e22ddc70682b844aa14de44baa01cf58718729
jianxieshui@gmail.com <jianxieshui@gmail.com@0318ff09-9246-0410-8118-259958648ee7>
2011-10-19 14:13:15 +0000

    修正由于SAE更改常用导致的数据库信息无法自动读入的问题

--- install/Mysql.php
+++ install/Mysql.php
@@ -4,26 +4,26 @@
 <?php //这里是专门为Sina App Engine做的判断 ?>
 <li>
 <label class="typecho-label"><?php _e('数据库地址'); ?></label>
-<input type="text" class="text" name="dbHost" value="<?php _v('dbHost', 'm'.$_SERVER['HTTP_MYSQLPORT'].'.mysql.sae.sina.com.cn'); ?>" />
+<input type="text" class="text" name="dbHost" value="<?php _v('dbHost', SAE_MYSQL_HOST_M); ?>" />
 <p class="description"><?php _e('这里Sina App Engine自动分配的数据库地址，请保留默认设置'); ?></p>
 </li>
 <li>
 <label class="typecho-label"><?php _e('数据库端口'); ?></label>
-<input type="text" class="text" name="dbPort" value="<?php _v('dbPort', $_SERVER['HTTP_MYSQLPORT']); ?>" />
+<input type="text" class="text" name="dbPort" value="<?php _v('dbPort', SAE_MYSQL_PORT); ?>" />
 <p class="description"><?php _e('如果您不知道此选项的意义, 请保留默认设置'); ?></p>
 </li>
 <li>
 <label class="typecho-label"><?php _e('数据库用户名'); ?></label>
-<input type="text" class="text" name="dbUser" value="<?php _v('dbUser', SAE_ACCESSKEY); ?>" />
+<input type="text" class="text" name="dbUser" value="<?php _v('dbUser', SAE_MYSQL_USER); ?>" />
 <p class="description"><?php _e('这里Sina App Engine自动分配的用户名，请保留默认设置'); ?></p>
 </li>
 <li>
 <label class="typecho-label"><?php _e('数据库密码'); ?></label>
-<input type="password" class="text" name="dbPassword" value="<?php _v('dbPassword', SAE_SECRETKEY); ?>" />
+<input type="password" class="text" name="dbPassword" value="<?php _v('dbPassword', SAE_MYSQL_PASS); ?>" />
 </li>
 <li>
 <label class="typecho-label"><?php _e('数据库名'); ?></label>
-<input type="text" class="text" name="dbDatabase" value="<?php _v('dbDatabase', 'app_'.$_SERVER['HTTP_APPNAME']); ?>" />
+<input type="text" class="text" name="dbDatabase" value="<?php _v('dbDatabase', SAE_MYSQL_DB); ?>" />
 <p class="description"><?php _e('请您指定数据库名称'); ?></p>
 </li>
 <?php //结束 ?>
